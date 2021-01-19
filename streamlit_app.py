1df770e344bd2f5afb202e91823a0561beb0c73f
joebk <59221652+joebk@users.noreply.github.com>
2021-01-19 20:47:55 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+from pycaret.classification import load_model, predict_model
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+def predict_quality():
+    
+    return "hey
+
+
+st.title('Wine Quality Classifier Web App')
+st.write('This is a web app to classify the quality of your wine based on\
+         several features that you can see in the sidebar. Please adjust the\
+         value of each feature. After that, click on the Predict button at the bottom to\
+         see the prediction of the classifier.')
